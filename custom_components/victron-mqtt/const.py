da5a098359bd3d476bb6a44503f641efd2aeb813
Tomer <57483589+tomer-w@users.noreply.github.com>
2025-06-13 18:18:05 +0300

    First checkin based on the extarcted code from the HA PR

---
+++ custom_components/victron-mqtt/const.py
@@ -0,0 +1,15 @@
+"""Constants for the victron-mqtt integration."""
+
+CONF_INSTALLATION_ID = "installation_id"
+CONF_MODEL = "model"
+CONF_SERIAL = "serial"
+
+DOMAIN = "victron-mqtt"
+
+DEVICE_MESSAGE = "device"
+SENSOR_MESSAGE = "sensor"
+
+PLACEHOLDER_PHASE = "{phase}"
+
+DEFAULT_HOST = "venus.local."
+DEFAULT_PORT = 1883
