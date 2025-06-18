a82f2427d6d5f4da6d88f614b5f389d33bd0a701
Tomer <57483589+tomer-w@users.noreply.github.com>
2025-06-19 00:53:55 +0300

    Move to latest package

--- custom_components/victron-mqtt/sensor.py
+++ custom_components/victron-mqtt/sensor.py
@@ -141,7 +141,7 @@ def _map_device_info(device: VictronVenusDevice) -> DeviceInfo:
     info: DeviceInfo = {}
     info["identifiers"] = {(DOMAIN, device.unique_id)}
     info["manufacturer"] = device.manufacturer
-    info["name"] = f"{device.name} ({device.unique_id})" if device.device_id != "0" else device.name
+    info["name"] = f"{device.name} (ID: {device.device_id})" if device.device_id != "0" else device.name
     info["model"] = device.model
     info["serial_number"] = device.serial_number
 
